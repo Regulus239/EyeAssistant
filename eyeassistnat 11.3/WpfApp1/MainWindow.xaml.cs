14ade24de3f50c50e40596010ebe8954897abc45
anqianqi1 <43048534+anqianqi1@users.noreply.github.com>
2018-11-03 17:06:56 -0400

    Add files via upload
    main frame of eyeassistant app. need to combine tobii with eye interaction

---
+++ eyeassistnat 11.3/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tobii.Interaction;
+using Tobii.Interaction.Wpf;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            email sec = new email();
+            sec.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\\Windows\\Notepad.exe");
+        }
+    }
+
+    public partial class App : Application
+    {
+        private Host _host;
+        private WpfInteractorAgent _wpfInteractorAgent;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host = new Host();
+            _wpfInteractorAgent = _host.InitializeWpfAgent();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ eyeassistnat 11.3/WpfApp1/email.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tobii.Interaction;
+using Tobii.Interaction.Wpf;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for email.xaml
+    /// </summary>
+    public partial class email : Window
+    {
+        public email()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public partial class Appemail : Application
+    {
+        private Host _hostemail;
+        private WpfInteractorAgent _wpfInteractorAgentemail;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _hostemail = new Host();
+            _wpfInteractorAgentemail = _hostemail.InitializeWpfAgent();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _hostemail.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
