d8b876354b10545d6a0b35a793569c52e03fee16
jsneal <jsneal@bu.edu>
2018-11-08 17:25:08 -0500

    Cleaned up variable names and function names.

--- ImportPythonEmailModules2/ImportPythonEmailModules2/Program.cs
+++ ImportPythonEmailModules2/ImportPythonEmailModules2/Program.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 // I need to try importing this into another module.
 // I should also run some better test cases on this method as I have only tested this on a few emails and am worried that it will not work for all.
 // I also need to clean up commented out code. I have it there for now because I am still testing this code to see it works with various test-case emails.
-// I could also possibly break the method into a few methods, making it more modular and easy to read.ated 
+
 namespace ImportPythonEmailModules
 {
     /// <summary> 
@@ -23,50 +23,52 @@ namespace ImportPythonEmailModules
     class Program
     {
         static void Main(string[] args)
-        {   // Proof that the function works->Build ImportPythonEmailModules2:
+        {   
+            // Proof that the function works->Build ImportPythonEmailModules2:
             List<Dictionary<string, string>> emailList = new List<Dictionary<string, string>>();
             string pythonEmailString;
             string[] pythonEmailArray;
             pythonEmailString = importPythonEmailString();
-            pythonEmailArray = string2StringArray(pythonEmailString);
-            emailList = emailString2ListDict(pythonEmailArray);
+            pythonEmailArray = separateEmailStrings(pythonEmailString);
+            emailList = createListOfEmailDicts(pythonEmailArray);
             // Then see that these console functions respond appropriately (feel free to index any which way you'd like using the list index and dictionary keys).
             Console.WriteLine(emailList[0]["From"]);
             Console.ReadKey();
         }
 
-       static List<Dictionary<string, string>> emailString2ListDict(string[] returnStrs)
+       static List<Dictionary<string, string>> createListOfEmailDicts(string[] unfixedEmailArray)
         {
-            char[] returnStrsChars2Trim = { '{', '}' }; // For getting rid of Python dictionary brackets
-            char[] trimQuotes = { '\'' }; // for getting rid of Python dictionary keys
-            string[] splitStrArrayChars = { "',", ", '" }; // for splitting entries in Python dictionary from each other
+            char[] dictBrackets = { '{', '}' }; // For getting rid of Python dictionary brackets
+            char[] singleQuote = { '\'' }; // for getting rid of Python dictionary keys
+            string[] emailArrayChars = { "',", ", '" }; // for splitting entries in Python dictionary from each other
 
             // String manipulation functions for extraction
             List<Dictionary<string, string>> emailList = new List<Dictionary<string, string>>(); // Final output of method to be sent to our UI
 
-            for (int i = 0; i<returnStrs.Length; i++)
+            for (int i = 0; i<unfixedEmailArray.Length; i++)
             {
-                string[] splitStrArray;
-                returnStrs[i] = returnStrs[i].Trim(returnStrsChars2Trim); // getting rid of Python dictionary brackets
+                string[] emailArray;
+                unfixedEmailArray[i] = unfixedEmailArray[i].Trim(dictBrackets); // getting rid of Python dictionary brackets
 
                 // Taking a particular dictionary (an email data set) and separating it into fields
-                splitStrArray = returnStrs[i].Split(splitStrArrayChars, StringSplitOptions.None);
+                emailArray = unfixedEmailArray[i].Split(emailArrayChars, StringSplitOptions.None);
 
-                // Each element of splitStrArray corresponds to a field of the email dictionary
-                splitStrArray[0] = splitStrArray[0].Replace("'Date': ", "").Trim(trimQuotes);
-                splitStrArray[1] = splitStrArray[1].Replace(" 'To': ", "").Trim(trimQuotes);
-                splitStrArray[2] = splitStrArray[2].Replace(" 'Content': ", "").Trim(trimQuotes);
-                splitStrArray[3] = splitStrArray[3].Replace(" 'From': ", "").Replace("From': ", "").Trim(trimQuotes);
-                splitStrArray[4] = splitStrArray[4].Replace(" 'Subject': ", "").Trim(trimQuotes);
+                // Each element of emailArray corresponds to a field of the email dictionary
+                emailArray[0] = emailArray[0].Replace("'Date': ", "").Trim(singleQuote);
+                emailArray[1] = emailArray[1].Replace(" 'To': ", "").Trim(singleQuote);
+                emailArray[2] = emailArray[2].Replace(" 'Content': ", "").Trim(singleQuote);
+                // This next one is kinda hack-y with the Replace("From': ", ""). Need to find a cleaner way.
+                emailArray[3] = emailArray[3].Replace(" 'From': ", "").Replace("From': ", "").Trim(singleQuote);
+                emailArray[4] = emailArray[4].Replace(" 'Subject': ", "").Trim(singleQuote);
 
                 // Creating C# email dictionary
                 Dictionary<string, string> singleEmailDict = new Dictionary<string, string>()
                                         {
-                                            {"Date",splitStrArray[0]},
-                                            {"To", splitStrArray[1]},
-                                            {"Content",splitStrArray[2]},
-                                            {"From",splitStrArray[3]},
-                                            {"Subject",splitStrArray[4]}
+                                            {"Date",emailArray[0]},
+                                            {"To", emailArray[1]},
+                                            {"Content",emailArray[2]},
+                                            {"From",emailArray[3]},
+                                            {"Subject",emailArray[4]}
                                         };
                 // Adding dictionary to list of dictionaries
                 emailList.Add(singleEmailDict);
@@ -87,61 +89,59 @@ namespace ImportPythonEmailModules
             string python = @"C:\Users\jsnea\Anaconda2\python.exe";
 
             // python app to call 
-            string myPythonApp = "C:\\Users\\jsnea\\Desktop\\ec601code\\Python_Email_Modules\\main.py";
+            string myPythonMain = "C:\\Users\\jsnea\\Desktop\\ec601code\\Python_Email_Modules\\main.py";
 
             // Create new process start info 
-            ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(python);
+            ProcessStartInfo python2Filepath = new ProcessStartInfo(python);
 
             // make sure we can read the output from stdout 
-            myProcessStartInfo.UseShellExecute = false;
-            myProcessStartInfo.RedirectStandardOutput = true;
+            python2Filepath.UseShellExecute = false;
+            python2Filepath.RedirectStandardOutput = true;
 
             // start python app with 3 arguments  
             // 1st arguments is pointer to itself,  
             // 2nd and 3rd are actual arguments we want to send 
-            myProcessStartInfo.Arguments = myPythonApp; // + " " + x + " " + y;
+            python2Filepath.Arguments = myPythonMain;
 
-            Process myProcess = new Process();
+            Process python2Process = new Process();
             // assign start information to the process 
-            myProcess.StartInfo = myProcessStartInfo;
+            python2Process.StartInfo = python2Filepath;
 
-            Console.WriteLine("Calling main.py");
+            //Console.WriteLine("Calling main.py"); (from old code, just keeping here for testing)
 
             // start the process 
-            myProcess.Start();
+            python2Process.Start();
 
             // Read the standard output of the app we called.  
             // in order to avoid deadlock we will read output first 
             // and then wait for process terminate: 
-            StreamReader myStreamReader = myProcess.StandardOutput;
+            StreamReader pythonMainStdOut = python2Process.StandardOutput;
             //string myString = myStreamReader.ReadLine();
 
             //if you need to read multiple lines, you might use: 
-            string myString = myStreamReader.ReadToEnd(); // horrible variable name (needs to be replaced)
-            string newString; // horrible variable name (needs to be replaced) for myString without random \r's and \n's from reading from stdout via Python
-            newString = myString.Replace("\\r", "").Replace("\\n", ""); // for myString without random \r's and \n's from reading from stdout via Python
+            string stdOutString = pythonMainStdOut.ReadToEnd(); // horrible variable name (needs to be replaced)
+            string fixedStdOutString; // horrible variable name (needs to be replaced) for myString without random \r's and \n's from reading from stdout via Python
+            fixedStdOutString = stdOutString.Replace("\\r", "").Replace("\\n", ""); // for myString without random \r's and \n's from reading from stdout via Python
 
             // wait exit signal from the app we called and then close it. 
-            myProcess.WaitForExit();
-            myProcess.Close();
+            python2Process.WaitForExit();
+            python2Process.Close();
 
-            return newString;
+            return fixedStdOutString;
         }
 
-        static string[] string2StringArray(string newString)
+        static string[] separateEmailStrings(string fixedStdOutString)
         {
             //There are a lot of char[]'s for various trimming steps
-            char[] chars2Trim = { '[', ']' }; // for getting rid of Python List brackets
-            char[] returnStrsChars2Trim = { '{', '}' }; // For getting rid of Python dictionary brackets
+            char[] listBrackets = { '[', ']' }; // for getting rid of Python List brackets
             char[] trimQuotes = { '\'' }; // for getting rid of Python dictionary keys
 
             // A few string[]'s for various splitting steps
-            string[] str2Split = { "}, " }; // For splitting dictioanries in the Python list from each other
-            string[] splitStrArrayChars = { "',", ", '" }; // for splitting entries in Python dictionary from each other
+            string[] endOfDictChars = { "}, " }; // For splitting dictioanries in the Python list from each other
 
             string[] returnStrs; // result of split with newString, probably not necessary
-            newString = newString.Trim(chars2Trim); // getting rid of Python list brackets
-            returnStrs = newString.Split(str2Split, StringSplitOptions.None); // getting rid of Python dictionary brackets
+            fixedStdOutString = fixedStdOutString.Trim(listBrackets); // getting rid of Python list brackets
+            returnStrs = fixedStdOutString.Split(endOfDictChars, StringSplitOptions.None); // getting rid of Python dictionary brackets
             return returnStrs;
         }
 
