9e456dcde46a21143d07925710a6ce8c8b2c1bf6
jsneal <jsneal@bu.edu>
2018-11-27 12:51:46 -0500

    This compiles, and I am able to initialize the properties I need (GazeAwareBehavior and HasGaze), but It does not open notepad.

--- ClickWithGazeButton/ClickWithGazeButton/MainWindow.xaml.cs
+++ ClickWithGazeButton/ClickWithGazeButton/MainWindow.xaml.cs
@@ -31,13 +31,14 @@ namespace ClickWithGazeButton
     public class Button1 : Grid
     {
         Grid buttonGrid;
+        GazeAwareBehavior buttonGridBehavior;
         //private Host _host;
-        //private Tobii.Interaction.Wpf.WpfInteractor _greetingInteractor;
+       // private Tobii.Interaction.Wpf.WpfInteractor _greetingInteractor;
         //private Tobii.Interaction.Wpf.WpfInteractorAgent _interactorAgent;
         //private Tobii.Interaction.WpfInteractorProvider _interactorProvider;
         //private string Button1Name;
         //public Tobii.Interaction.Rectangle Button1Rect;
-        /*public void onGazeEnters()
+        public void OnGazeEnters()
         {
             System.Diagnostics.Process.Start("C:\\Windows\\Notepad.exe");
         }
@@ -45,20 +46,22 @@ namespace ClickWithGazeButton
         {
 
         }
-        */
         public Button1()
         {
             buttonGrid = new Grid();
+            buttonGridBehavior = new GazeAwareBehavior(Tobii.Interaction.Framework.GazeAwareMode.Normal);
+            buttonGridBehavior.HasGaze(OnGazeEnters);
             //Tobii.Interaction.Rectangle ButtonRect = Button1Rect;
             //Button1Name = "Button1";
             //_host = new Host();
             //_interactorProvider = new Tobii.Interaction.WpfInteractorProvider
            //_interactorAgent = new Tobii.Interaction.Wpf.WpfInteractorAgent(Button1Name, _interactorProvider); // Button1Str is the name of the Grid
             //_greetingInteractor = _interactorAgent.AddInteractorFor(this); // Need to  get rid of "this"
-            /*_greetingInteractor
+            /*buttonGrid
                 .WithGazeAware()
                 .HasGaze(onGazeEnters) // onGazeEnters is an action
-                .LostGaze(onGazeLeaves); // onGazeLeaves is an action*/
+                .LostGaze(onGazeLeaves); // onGazeLeaves is an action
+                */
         }
     }
 }
