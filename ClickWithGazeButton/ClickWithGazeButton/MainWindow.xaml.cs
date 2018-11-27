8dadf548cd488d2861a2201ed3d83f5593139c22
jsneal <jsneal@bu.edu>
2018-11-27 14:21:58 -0500

    Now the button I have created will open notepad and turn magenta when you look inside of it.

--- ClickWithGazeButton/ClickWithGazeButton/MainWindow.xaml.cs
+++ ClickWithGazeButton/ClickWithGazeButton/MainWindow.xaml.cs
@@ -26,31 +26,50 @@ namespace ClickWithGazeButton
             InitializeComponent();
             
         }
+
+        public void OnGazeEnters()
+        {
+            System.Diagnostics.Process.Start("C:\\Windows\\Notepad.exe");
+        }
+
+        private void Instruction_OnHasGazeChanged(object sender, RoutedEventArgs e)
+        {
+            var textBlock = e.Source as Grid;
+            if (null == textBlock) { return; }
+            OnGazeEnters();
+            /*var hasGaze = textBlock.GetHasGaze();
+            model.NotifyInstructionHasGazeChanged(hasGaze);*/
+        }
     }
 
     public class Button1 : Grid
     {
         Grid buttonGrid;
         GazeAwareBehavior buttonGridBehavior;
+        bool openNotepad;
         //private Host _host;
        // private Tobii.Interaction.Wpf.WpfInteractor _greetingInteractor;
         //private Tobii.Interaction.Wpf.WpfInteractorAgent _interactorAgent;
         //private Tobii.Interaction.WpfInteractorProvider _interactorProvider;
         //private string Button1Name;
         //public Tobii.Interaction.Rectangle Button1Rect;
-        public void OnGazeEnters()
-        {
-            System.Diagnostics.Process.Start("C:\\Windows\\Notepad.exe");
-        }
+
         public void onGazeLeaves()
         {
 
         }
-        public Button1()
+        /*private void Instruction_OnHasGazeChanged(object sender, RoutedEventArgs e)
         {
+            var textBlock = e.Source as Grid;
+            if (null == textBlock) { return; }
+            var hasGaze = textBlock.GetHasGaze();
+            model.NotifyInstructionHasGazeChanged(hasGaze);
+        }*/
+        public Button1()
+        {   
             buttonGrid = new Grid();
             buttonGridBehavior = new GazeAwareBehavior(Tobii.Interaction.Framework.GazeAwareMode.Normal);
-            buttonGridBehavior.HasGaze(OnGazeEnters);
+            //buttonGridBehavior.HasGaze(HasGazeChangedEventArgs);
             //Tobii.Interaction.Rectangle ButtonRect = Button1Rect;
             //Button1Name = "Button1";
             //_host = new Host();
