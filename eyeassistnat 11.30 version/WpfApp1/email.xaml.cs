76b3c1f60469f5b7e8fc2ca4d34056d8d91c19f6
anqianqi1 <43048534+anqianqi1@users.noreply.github.com>
2018-11-30 19:40:24 -0500

    Add files via upload

---
+++ eyeassistnat 11.30 version/WpfApp1/email.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tobii.Interaction;
+using Tobii.Interaction.Wpf;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for email.xaml
+    /// </summary>
+    public partial class email : Window
+    {
+        bool HasGaze_Exit;
+        public email()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var grid = e.Source as Grid;
+            if (null == grid) { return; } // if the named "EventSetter" Event from MainWindow.xaml is not called.
+            if (HasGaze_Exit)
+            {
+                HasGaze_Exit = false; // You're leaving the button.
+            }
+            else
+            {
+                HasGaze_Exit = true;        // You're entering the button.
+                this.Close();
+            }
+        }
+
+    }
+
+    public partial class Appemail : Application
+    {
+        private Host _hostemail;
+        private WpfInteractorAgent _wpfInteractorAgentemail;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _hostemail = new Host();
+            _wpfInteractorAgentemail = _hostemail.InitializeWpfAgent();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _hostemail.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
